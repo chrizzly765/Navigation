e4abbfe7ce2ac3cee6206ee4c2b0d0c1db792cf6
gabrielan56214 <gabrielan56214@th-nuernberg.de>
2016-05-22 17:16:42 +0200

    new structure 2nd part

--- Helper.java
+++ Helper.java
@@ -49,4 +49,11 @@ import fu.keys.LSIClassCentre;
 
         return mapDefaultSpeed.get(token);
     }
+	
+    public static double getLinkCostsInSeconds(double distance, double speed) {
+
+        // 30km/h = 30000m / 3600s = 8,333m/s
+        // time = distance/speed => 70m/s : 8,333m/s = 8,4s
+        return (distance / ((speed * 1000) / 3600));
+    }
 }
