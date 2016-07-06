63ff2b6a5f73e5f27bbc4f8ddd220a1ca3fced9e
aufotuni56409 <aufotuni56409@th-nuernberg.de>
2016-07-06 12:36:50 +0200

    route_turns

--- Route.java
+++ Route.java
@@ -65,7 +65,7 @@ public class Route
 			int diff;
 
 			for (int i=0;i<route.length; i++) {
-				
+
 				if(route[i].domainID == 0) {
 					continue;
 				}
@@ -92,6 +92,72 @@ public class Route
 			return true;
 		}
 
+		// public boolean printTurns() throws FileNotFoundException {
+		//
+		// 	pwTurns = new PrintWriter(TURNS_TXT);
+		//
+		// 	String strLog = "";
+		// 	boolean temp = false;
+		// 	int linkID, domainID, nextDomainID, nextLinkID, alpha, beta, differenz;
+		// 	String domainName, nextDomainName, txt;
+		//
+		// 	//start domain
+		// 	strLog += "Sie sind auf der " + Navigate.nd.getDomainName(Navigate.nd.getDomainID(route[1].linkIDToPredecessor)) + eol;
+		//
+		// 	for (int i=1;i<route.length-1; i++) {
+		//
+		// 		linkID = route[i].linkIDToPredecessor;
+		// 		domainID = Navigate.nd.getDomainID(route[i].linkIDToPredecessor);
+		// 		domainName = Navigate.nd.getDomainName(domainID);
+		// 		nextDomainID = Navigate.nd.getDomainID(route[i+1].linkIDToPredecessor);
+		// 		nextDomainName =  Navigate.nd.getDomainName(nextDomainID);;
+		// 		alpha = Navigate.nd.getNorthAngleTo(linkID);
+		// 		nextLinkID = route[i+1].linkIDToPredecessor;
+		// 		beta = Navigate.nd.getNorthAngleFrom(nextLinkID);
+		// 		differenz = Math.abs(Math.abs(alpha)-Math.abs(beta));
+		// 		txt = "";
+		//
+		// 		if (differenz > 10 && differenz <= 50 ){
+		// 			txt = "leicht ";
+		// 		}
+		// 		else if(differenz > 140){
+		// 			txt = "scharf ";
+		// 		}
+		//
+		// 		if (domainName.equals(nextDomainName)) {
+		// 			if (temp == false){
+		// 				strLog += "Bleiben Sie auf der " + nextDomainName + eol;
+		// 				temp = true;
+		// 			}
+		// 		}
+		// 		else if(differenz <= 10){
+		// 			strLog += "Fahren Sie weiter gerade aus auf die " + nextDomainName + eol;
+		// 			temp = false;
+		// 		}
+		// 		else if((beta < 0 && alpha < 0)||(beta > 0 && alpha > 0)) {
+		// 			if(alpha > beta){
+		// 				strLog += "Biegen Sie bitte " + txt + "links ab, in " + nextDomainName + eol;
+		// 			}
+		// 			else if(alpha < beta) {
+		// 				strLog += "Biegen Sie bitte " + txt + "rechts ab, in " + nextDomainName + eol;
+		// 			}
+		// 			temp = false;
+		// 		}
+		// 		else if(alpha > 0 && beta < 0){
+		// 			strLog += "Biegen Sie bitte " + txt + " links ab, in " + nextDomainName + eol;
+		// 			temp = false;
+		// 		}
+		// 		else if (alpha < 0 && beta > 0){
+		// 			strLog += "Biegen Sie bitte " + txt + " rechts ab, in " + nextDomainName + eol;
+		// 			temp = false;
+		// 		}
+		// 	}
+		// 	strLog += "Sie haben Ihr Ziel erreicht!";
+		// 	pwTurns.println(strLog);
+		// 	pwTurns.close();
+		// 	return true;
+		//
+		// }
 		public boolean printTurns() throws FileNotFoundException {
 
 			pwTurns = new PrintWriter(TURNS_TXT);
@@ -99,10 +165,8 @@ public class Route
 			String strLog = "";
 			boolean temp = false;
 			int linkID, domainID, nextDomainID, nextLinkID, alpha, beta, differenz;
-			String domainName, nextDomainName, txt;
-			
-			//start domain
-			strLog += "Sie sind auf der " + Navigate.nd.getDomainName(Navigate.nd.getDomainID(route[1].linkIDToPredecessor)) + eol;
+			String domainName, nextDomainName, txtStaerke, txtLR, ziel;
+			ziel = Navigate.nd.getDomainName(Navigate.nd.getDomainID(route[route.length-1].linkIDToPredecessor));
 
 			for (int i=1;i<route.length-1; i++) {
 
@@ -111,48 +175,57 @@ public class Route
 				domainName = Navigate.nd.getDomainName(domainID);
 				nextDomainID = Navigate.nd.getDomainID(route[i+1].linkIDToPredecessor);
 				nextDomainName =  Navigate.nd.getDomainName(nextDomainID);;
-				alpha = Navigate.nd.getNorthAngleTo(linkID);
+				alpha = Navigate.nd.getNorthAngleFrom(linkID);
 				nextLinkID = route[i+1].linkIDToPredecessor;
-				beta = Navigate.nd.getNorthAngleFrom(nextLinkID);
-				differenz = Math.abs(Math.abs(alpha)-Math.abs(beta));
-				txt = "";
-				
-				if (differenz > 10 && differenz <= 50 ){
-					txt = "leicht ";
+				beta = Navigate.nd.getNorthAngleTo(nextLinkID);
+				differenz = 0;
+				txtStaerke = "";
+
+				//Richtung bestimmen
+				if (beta > 0) txtLR = "links ab, in ";
+				else txtLR = "rechts ab, in ";
+
+				//Differenz bestimmen
+				//wenn gleiche vorzeichen
+				if ((beta < 0 && alpha < 0)||(beta > 0 && alpha > 0)){
+					differenz = 360 - (Math.abs(alpha) + (180 - Math.abs(beta)));
 				}
-				else if(differenz > 140){
-					txt = "scharf ";
+				//wenn unterschiedliche vorzeichen
+				else if ((alpha > 0 && beta < 0)||(alpha < 0 && beta > 0)){
+					differenz = 360 - (Math.abs(alpha) + Math.abs(beta));
+				}
+				//differenz
+				if (differenz > 10 && differenz <= 50 ){
+					txtStaerke = "leicht";
+				}
+				else if (differenz > 140 && differenz < 150){
+					txtStaerke = "scharf";
+				}
+				//zwischen 50 und 140
+				else {
+					txtStaerke = "";
 				}
 
+
 				if (domainName.equals(nextDomainName)) {
-					if (temp == false){
-						strLog += "Bleiben Sie auf der " + nextDomainName + eol;
-						temp = true;
-					}
-				}
+							strLog += "Bleiben Sie auf der " + nextDomainName + eol;
+						}
 				else if(differenz <= 10){
-					strLog += "Fahren Sie weiter gerade aus auf die " + nextDomainName + eol;
-					temp = false;
+						strLog += "Fahren Sie weiter gerade aus auf die " + nextDomainName + eol;
+						// temp = false;
 				}
-				else if((beta < 0 && alpha < 0)||(beta > 0 && alpha > 0)) {
-					if(alpha > beta){
-						strLog += "Biegen Sie bitte " + txt + "links ab, in " + nextDomainName + eol;
-					}
-					else if(alpha < beta) {
-						strLog += "Biegen Sie bitte " + txt + "rechts ab, in " + nextDomainName + eol;
-					}
-					temp = false;
+				else {
+					strLog += "Biegen Sie bitte " + txtStaerke + " " + txtLR + " " + nextDomainName + eol;
 				}
-				else if(alpha > 0 && beta < 0){
-					strLog += "Biegen Sie bitte " + txt + " links ab, in " + nextDomainName + eol;
-					temp = false;
-				}
-				else if (alpha < 0 && beta > 0){
-					strLog += "Biegen Sie bitte " + txt + " rechts ab, in " + nextDomainName + eol;
-					temp = false;
-				}
+
+
+				// strLog += "---------TEST--------" + eol;
+				// strLog += " alpha " + alpha + " beta " + beta + " differenz " + differenz + eol;
+				// strLog +=" txtStaerke: " + txtStaerke + " txtLR " + txtLR + " Straße: " + nextDomainName + eol;
+				// strLog += "---------TEST--------" + eol;
+
 			}
-			strLog += "Sie haben Ihr Ziel erreicht!";
+			strLog += "Sie haben die " + ziel + " erreicht!";
 			pwTurns.println(strLog);
 			pwTurns.close();
 			return true;
